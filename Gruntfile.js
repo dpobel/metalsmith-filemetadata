0c4e1604374413e8fd2b69a76971d485126d46bb
Damien Pobel <dp@ez.no>
2014-05-14 22:05:45 +0200

    Added a jshint check

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,11 +18,18 @@ module.exports = function(grunt) {
                 src: ['tests/*.js']
             },
         },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: ['*.js', 'tests/*.js'],
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    grunt.registerTask('test', ['mochaTest']);
+    grunt.registerTask('test', ['mochaTest', 'jshint']);
     grunt.registerTask('default', ['test']);
 };
