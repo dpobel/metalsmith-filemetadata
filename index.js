967b5620b8b76d9d6230802ce5b012c1d4a875ea
Levi Wheatcroft <levi@wheatcrofts.com.au>
2017-01-23 17:45:02 +0800

    * fix: not calling back  * feature: interpolation  * added interpolation test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var multimatch = require('multimatch');
+var interpolate = require('metalsmith-interpolate').default;
 
 /**
  * Sets some metadata on each file depending a pattern
@@ -13,12 +14,14 @@ module.exports = function (rules) {
   return function (files, metalsmith, done) {
     if (!rules) return done();
     rules.forEach((rule) => {
-      multimatch(Object.keys(files), rule.pattern).forEach((file) => {
+      multimatch(Object.keys(files), rule.pattern).forEach((src) => {
+        let file = files[src];
         Object.keys(rule.metadata).forEach((key) => {
           if (rule.preserve && file.hasOwnProperty(key)) return;
-          file[key] = rule.metadata[key];
+          file[key] = interpolate(rule.metadata[key], src, files);
         });
       });
     });
+    done();
   };
 };
